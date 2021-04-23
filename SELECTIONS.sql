2008638006e7cd579fe839d7bebfa9816d7e3f3a
almeidagit <msnpaulo@msn.com>
2021-04-22 23:16:34 -0300

    SELECTIONS UPDATE

--- SELECTIONS.sql
+++ SELECTIONS.sql
@@ -1,15 +1,28 @@
+/* Retorna MAIOR valor de um range */
+SELECT to_char(DATA_HORA,'dd/mm/yyyy hh24:mi:ss'),MAXIMA,MINIMA 
+FROM INDICE  
+WHERE  TO_CHAR(DATA_HORA,'hh24') 
+BETWEEN '09' AND '10';
+
+/* Retorna MAIOR valor de um Período */
+SELECT MAX(MAXIMA) 
+FROM INDICE  
+WHERE  TO_CHAR(DATA_HORA,'hh24') 
+BETWEEN '09' AND '10';
 
 /*Retorna o Rank a partir do maior preço do Período */
 SELECT 
-    to_char(DATA_HORA,'cc dd/mm/yyyy hh24:mi:ss'), MAXIMA,
+    to_date(DATA_HORA,'dd/mm/yyyy hh24:mi:ss'), MAXIMA,
 	RANK() OVER (ORDER BY MAXIMA DESC) 
-FROM 
-	INDICE;
+FROM INDICE;
 
 /*Retorna o Rank a partir do menor preço do Período */
 SELECT 
-	to_char(DATA_HORA,'cc dd/mm/yyyy hh24:mi:ss'), MINIMA,
+	to_char(DATA_HORA,'dd/mm/yyyy hh24:mi:ss'), MINIMA,
 	RANK() OVER (ORDER BY MINIMA) 
-FROM 
-	INDICE;
+FROM INDICE;
+
+
+
+
 
